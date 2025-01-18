8f74322b2320c0358b13edcddc2cf219505e9d7b
Patrick K. Byers <pbyers413@gmail.com>
2025-01-17 21:33:44 -0500

    script changes
    https://youtu.be/1mM73u1tvpU?si=EWST6MtSu2R0ufkI

--- project/CardManager.gd
+++ project/CardManager.gd
@@ -4,6 +4,7 @@ const COLLISION_MASK_CARD = 1
 
 var card_being_dragged : Node2D
 var screen_size : Vector2
+var is_hovering_on_card
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,9 +28,48 @@ func _input(event: InputEvent) -> void:
 			# Raycast check for card
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card
+				start_drag(card)
 		else:
-			card_being_dragged = null
+			finish_drag()
+
+func start_drag(card: Node2D):
+	card.scale = Vector2(1, 1)
+	card_being_dragged = card
+
+func finish_drag():
+	card_being_dragged.scale = Vector2(1.05, 1.05)
+	card_being_dragged = null
+
+
+func connect_card_signals(card):
+	card.connect("hovered", on_hover_over_card)
+	card.connect("hovered_off", on_hover_off_card)
+
+
+func on_hover_over_card(card):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(card, true)
+
+
+func on_hover_off_card(card):
+	if !card_being_dragged:
+		highlight_card(card, false)
+		var new_card_hovered = raycast_check_for_card()
+		if new_card_hovered:
+			highlight_card(new_card_hovered, true)
+		else:
+			is_hovering_on_card = false
+
+
+func highlight_card(card: Node2D, hovered: bool):
+	if hovered:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1, 1)
+		card.z_index = 1
+
 
 func raycast_check_for_card() -> Variant:
 	var space_state = get_world_2d().direct_space_state
@@ -39,6 +79,21 @@ func raycast_check_for_card() -> Variant:
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
-		var card : Node2D = result[0].collider.get_parent()
+		#var card : Node2D = result[0].collider.get_parent()
+		var card = get_card_with_highest_z_index(result)
 		return card
 	return null
+
+
+func get_card_with_highest_z_index(cards: Array):
+	var highest_z_card : Node2D = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+
+	# Loop over other cards
+	for i in range(1, cards.size()):
+		var current_card : Node2D = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+
+	return highest_z_card
