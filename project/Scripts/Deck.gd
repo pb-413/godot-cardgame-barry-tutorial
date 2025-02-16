dc790ee4d5d02fd03ad1948d2745faefa69db3cb
Patrick K. Byers <pbyers413@gmail.com>
2025-02-16 17:37:45 -0500

    init - move project files around, add deck resource, outline deck object and script
    2:05 of https://youtu.be/riafP7MtvmQ?si=IrNjwHy4YW2-ShC2
    
    next: raycast so deck can see click, create interaction/input manager

---
+++ project/Scripts/Deck.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+var player_deck = ["Knight", "Knight", "Knight",]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func draw_card():
+	print("draw card")
