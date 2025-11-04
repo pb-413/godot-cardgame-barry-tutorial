939274341ec90b401369811c4b16b12651307c9c
Patrick K. Byers <pbyers413@gmail.com>
2025-11-04 12:24:12 -0500

    increase reliability/fix bug with running _on_peer_connected: use a higher port number; add export of enemy_field_scene, utilize in multiplayer setup

--- project/Scripts/Multiplayer.gd
+++ project/Scripts/Multiplayer.gd
@@ -2,13 +2,30 @@ extends Node2D
 
 # The port that the server listens for communications on.
 const SERVER_ADDRESS = "localhost"
-const PORT = 123
+# Higher number resulted in more reliable use
+# Context: "Ports below 1024 are considered privileged" -Google search
+const PORT = 9999
+# Troubleshooting in CMD:
+# - netstat -ano | findstr <port number>
+# - tasklist | findstr <process id>
+# - taskkill /F /PID <process id>
 
 # Create multiplayer object called 'peer' using ENet networking library.
 var peer = ENetMultiplayerPeer.new()
 
 @export var player_field_scene: PackedScene
+@export var enemy_field_scene: PackedScene
 
+# To host safely and repeatitively,
+# do I have to teardown the server during quit? -> No, use higher port number!
+#func _ready():
+    #get_tree().set_auto_accept_quit(false)
+#
+#func _notification(what: int) -> void:
+    #if what == NOTIFICATION_WM_CLOSE_REQUEST:
+        ## Shutdown networking server.
+        #multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new()
+        #get_tree().quit()
 
 func _dissable_button(button: Button):
     button.disabled = true
@@ -21,7 +38,8 @@ func dissable_buttons():
 
 
 func _on_peer_connected(peer_id):
-    print("Player joined!")
+    var enemy_scene = enemy_field_scene.instantiate()
+    add_child(enemy_scene)
 
 func _on_host_button_pressed() -> void:
     dissable_buttons()
@@ -49,3 +67,6 @@ func _on_join_button_pressed() -> void:
 
     var player_scene = player_field_scene.instantiate()
     add_child(player_scene)
+
+    var enemy_scene = enemy_field_scene.instantiate()
+    add_child(enemy_scene)
